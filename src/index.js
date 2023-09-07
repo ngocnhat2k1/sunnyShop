b8bff1d85c0ceac2be2551a738637aaad1624f6a
ngocnhat2k1 <1951120116@sv.ut.edu.vn>
2023-09-07 23:51:33 +0700

    rebuild

--- src/index.js
+++ src/index.js
@@ -11,14 +11,10 @@ root.render(
     <Helmet>
       <meta charSet="utf-8" />
       <title>Huong Duong Shop</title>
-      <link rel="canonical" href="http://mysite.com/example" />
     </Helmet>
     <App />
   </React.StrictMode>
   // <App />
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
